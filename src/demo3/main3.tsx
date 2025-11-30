c00931341f9ca6b3c1cfb0f3121613b7414ed40a
zhoumingle <2293188960@qq.com>
2025-11-30 19:05:52 +0800

    增加demo

--- src/demo3/main3.tsx
+++ src/demo3/main3.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './demo3/App3.tsx'
+import './index3.css'
+import App from './App3.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +8,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 5177,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        demo3: resolve(__dirname, 'demo3.html'),
+      },
+    },
   },
 })
